cb0801fbdbc230195703b6178b5146e8294c5f34
Risa Goto <omusubi5656dup@gmail.com>
2025-03-01 20:25:35 +0900

    test:btn color

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,11 +2,16 @@
 <html>
 <head>
     <title>テストページ</title>
+    <!-- Tailwind CSSを読み込み -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <h1>テストページへようこそ</h1>
     <div>
-        <!-- ここにコンテンツを追加 -->
+        <!-- グリーンボタン -->
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            クリック
+        </button>
     </div>
 </body>
 </html>
