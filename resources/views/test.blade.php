1d7008fc7b39b4e6c95afac15101772a140e603d
Risa Goto <omusubi5656dup@gmail.com>
2025-03-01 19:55:01 +0900

    test:エスケープ処理

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,19 +5,8 @@
 </head>
 <body>
     <h1>テストページへようこそ</h1>
-    
     <div>
         <!-- ここにコンテンツを追加 -->
     </div>
-
-    @if(Auth::check())
-        <div>
-            {{ Auth::user()->name }}さん、こんにちは。
-        </div>
-    @else
-        <div>
-            ゲストさん、こんにちは。
-        </div>
-    @endif
 </body>
-</html> 
+</html>
