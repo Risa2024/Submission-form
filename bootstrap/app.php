2313b998e9436fede12dde2fea76b800614249f4
Risa Goto <omusubi5656dup@gmail.com>
2025-03-04 15:14:52 +0900

    add:auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware)
      {
         $middleware->alias([
-            'admin' =>\App\Http\Middleware\RoleMiddleware::class
+            'admin' =>\App\Http\Middleware\RoleMiddleware::class,
+            'auth' =>\Illuminate\Auth\Middleware\Authenticate::class
         ]);
     })
 
