982892edb77187a57cea3c7daec03a00cc3f772a
Risa Goto <omusubi5656dup@gmail.com>
2025-03-04 16:50:46 +0900

    add:個別表示用viewファイル

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('投稿詳細') }}
+            個別表示
         </h2>
     </x-slot>
 
     <div class="max-w-7xl mx-auto px-6">
-        <div class="mt-4 p-8 bg-white w-full rounded-2xl">
-            <h1 class="p-4 text-lg font-semibold">
+        <div class="mt-4 p-4">
+            <h1 class="text-lg font-semibold">
                 {{ $post->title }}
             </h1>
             <hr class="w-full">
-            <p class="mt-4 p-4">
+            <p class="mt-4 whitespace-pre-line">
                 {{ $post->body }}
             </p>
-            <div class="p-4 text-sm font-semibold">
+            <div class="text-sm font-semibold flex flex-row-reverse">
                 <p>
                     {{$post->created_at}} / {{$post->user->name}}
                 </p>
             </div>
         </div>
     </div>
-</x-app-layout> 
+</x-app-layout>
