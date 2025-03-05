955e77630e9df428ad28c45c1ae841f8610a661c
Risa Goto <omusubi5656dup@gmail.com>
2025-03-05 14:08:38 +0900

    add:venderコピー

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(10);
         return view('dashboard', compact('posts'));
     }
 }
