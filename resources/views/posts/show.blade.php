eabde6642392530fc62733c24a41e13f634933a9
Risa Goto <omusubi5656dup@gmail.com>
2025-03-05 12:34:39 +0900

    fix:編集ページカラム

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,24 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl mx-auto px-6">
-        <div class="mt-4 p-4">
+    <div class="max-auto px-6">
+    <div class="mt-4 p-8 bg-white w-full rounded-2xl">
             <h1 class="text-lg font-semibold">
             {{ $post->title }}
             </h1>
-            <div class="text-right flex">
-            <a href="{{ route('posts.edit', $post) }}" class="flex-1">
+                 <div class="text-right flex">
+                <a href="{{ route('posts.edit', $post) }}" class="flex-1">
                 <x-primary-button>
                     編集
                 </x-primary-button>
-            </a>
-            <form method="post" action="{{ route('posts.destroy', $post) }}">
+                </a>
+                <form method="post" action="{{ route('posts.destroy', $post) }}">
                 @csrf
                 @method('delete')
                 <x-primary-button class="bg-red-700 ml-2">
                     削除
                 </x-primary-button>
-            </form>
+                </form>
             </div>
             <hr class="w-full">
             <p class="mt-4 whitespace-pre-line">
