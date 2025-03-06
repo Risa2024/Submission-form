d37a0262d3ca37b9d68ea6ba646f105284cd86d4
Risa Goto <omusubi5656dup@gmail.com>
2025-03-06 23:39:57 +0900

    fix:migrationエラー

--- database/migrations/2025_03_06_003148_modyfy_posts_table.php
+++ database/migrations/2025_03_06_003148_modyfy_posts_table.php
@@ -1,23 +1,32 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIdColumnToPostsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('user_id')
-                  ->constrained()           // usersテーブルと紐付け
-                  ->onDelete('cascade');    // ユーザーが削除されたら投稿も削除
+            //$table->dropForeign(['user_id']);
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
         });
     }
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+
         });
     }
-} 
+};
