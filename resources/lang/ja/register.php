66052b74d2e6618b4e62c684d940ead29a4974fe
Risa Goto <omusubi5656dup@gmail.com>
2025-03-15 16:54:31 +0900

    add:言語設定

---
+++ resources/lang/ja/register.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => '名前',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+    'password_confirmation' => 'パスワード（確認）',
+    'register' => '登録',
+    'already_registered' => 'すでに登録済みですか？',
+]; 
